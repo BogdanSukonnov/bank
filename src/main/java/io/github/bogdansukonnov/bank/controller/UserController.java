641da1f91684d618fd34023a94473dd95aec1540
Bogdan Sukonnov <bogdan.sukonnov@gmail.com>
2021-03-18 15:35:14 +0300

    swagger

--- src/main/java/io/github/bogdansukonnov/bank/controller/UserController.java
+++ src/main/java/io/github/bogdansukonnov/bank/controller/UserController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,6 +15,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 @Log4j2
+@RequestMapping("api")
 public class UserController {
 
     @NonNull
