b0ef1ae4d96fc44644a69782df665fa1efaf9ccd
Bogdan Sukonnov <bogdan.sukonnov@gmail.com>
2021-03-20 14:31:38 +0300

    fix serialization

--- src/main/java/io/github/bogdansukonnov/bank/dto/UserDto.java
+++ src/main/java/io/github/bogdansukonnov/bank/dto/UserDto.java
@@ -1,11 +1,32 @@
 package io.github.bogdansukonnov.bank.dto;
 
 import lombok.Data;
+import lombok.NonNull;
+import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
 @Data
 @SuperBuilder
+@ToString
 public class UserDto extends UserNoIdDto {
-
+    @NonNull
     private final String id;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        UserDto userDto = (UserDto) o;
+
+        return id.equals(userDto.id);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + id.hashCode();
+        return result;
+    }
 }
--- src/main/java/io/github/bogdansukonnov/bank/dto/UserNoIdDto.java
+++ src/main/java/io/github/bogdansukonnov/bank/dto/UserNoIdDto.java
@@ -1,13 +1,16 @@
 package io.github.bogdansukonnov.bank.dto;
 
 import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Data
 @SuperBuilder
+@RequiredArgsConstructor
 public class UserNoIdDto {
-
+    @NonNull
     private final String firstName;
-
+    @NonNull
     private final String lastName;
 }
